1bf6b2d43c8a9ddb741884a4f1c00580ea571f4d
Mod4 <omarelfarok24135@gmail.com>
2026-04-29 15:05:50 +0400

    Update LCAppListView.swift

--- LiveContainerSwiftUI/Views/AppList/LCAppListView.swift
+++ LiveContainerSwiftUI/Views/AppList/LCAppListView.swift
@@ -1680,6 +1680,7 @@ func setMode(_ mode: AppLaunchMode) {
                 var containerName : String? = nil
                 var openURL : String? = nil
                 var forceJIT: Bool? = nil
+                var openUrl: String? = nil
                 for queryItem in components.queryItems ?? [] {
                     if queryItem.name == "bundle-name", let bundleId1 = queryItem.value {
                         bundleId = bundleId1
@@ -1694,9 +1695,17 @@ func setMode(_ mode: AppLaunchMode) {
                         } else if forceJIT1 == "false" {
                             forceJIT = false
                         }
+                    } else if queryItem.name == "open-url",
+                              let encoded = queryItem.value,
+                              let decodedData = Data(base64Encoded: encoded),
+                              let decodedUrl = String(data: decodedData, encoding: .utf8) {
+                        openUrl = decodedUrl
                     }
                 }
                 if let bundleId, bundleId != "ui"{
+                    if let openUrl {
+                        UserDefaults.standard.setValue(openUrl, forKey: "launchAppUrlScheme")
+                    }
                     Task { await launchAppWithBundleId(bundleId: bundleId, container: containerName, openURL: openURL, forceJIT: forceJIT) }
                 }
             }
