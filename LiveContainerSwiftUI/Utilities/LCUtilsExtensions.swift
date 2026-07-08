1f3e682469d984bb64828e950f931ffab4a2020e
Mod4 <omarelfarok24135@gmail.com>
2026-07-08 15:26:34 +0400

    Update LCUtilsExtensions.swift

--- LiveContainerSwiftUI/Utilities/LCUtilsExtensions.swift
+++ LiveContainerSwiftUI/Utilities/LCUtilsExtensions.swift
@@ -320,13 +320,19 @@ extension LCUtils {
                     return false
                 }
                 // check if stosdebug is already running
+                let currentScheme = LCUtils.appUrlScheme()?.lowercased()
                 var freeScheme = LCSharedUtils.getContainerUsingLCScheme(withFolderName: appToLaunch.uiDefaultDataFolder)
+                if freeScheme?.lowercased() == currentScheme {
+                    freeScheme = nil
+                }
                 
                 if(freeScheme == nil) {
                     // if not, try to find a free lc
                     forEachInstalledLC(isFree: true) { scheme, shouldBreak in
-                        freeScheme = scheme
-                        shouldBreak = true
+                        if scheme.lowercased() != currentScheme {
+                            freeScheme = scheme
+                            shouldBreak = true
+                        }
                     }
                 }
                 guard let freeScheme else {
@@ -336,7 +342,7 @@ extension LCUtils {
                 
                 let launchURL = URL(string: "\(freeScheme)://open-url?url=\(encodedStr)")!
                 LCUtils.appGroupUserDefault.set(freeScheme, forKey: "LCLaunchExtensionScheme")
-                LCUtils.appGroupUserDefault.set(appToLaunch.appInfo.relativeBundlePath, forKey: "LCLaunchExtensionBundleID")
+                LCUtils.appGroupUserDefault.set(appToLaunch.appInfo.relativeBundlePath ?? appToLaunch.bundleIdentifier, forKey: "LCLaunchExtensionBundleID")
                 LCUtils.appGroupUserDefault.set(Date.now, forKey: "LCLaunchExtensionLaunchDate")
                 onServerMessage?("JIT acquisition will continue in another LiveContainer.")
                 
@@ -381,13 +387,19 @@ extension LCUtils {
                     return false
                 }
                 // check if stikdebug is already running
+                let currentScheme = LCUtils.appUrlScheme()?.lowercased()
                 var freeScheme = LCSharedUtils.getContainerUsingLCScheme(withFolderName: appToLaunch.uiDefaultDataFolder)
+                if freeScheme?.lowercased() == currentScheme {
+                    freeScheme = nil
+                }
                 
                 if(freeScheme == nil) {
                     // if not, try to find a free lc
                     forEachInstalledLC(isFree: true) { scheme, shouldBreak in
-                        freeScheme = scheme
-                        shouldBreak = true
+                        if scheme.lowercased() != currentScheme {
+                            freeScheme = scheme
+                            shouldBreak = true
+                        }
                     }
                 }
                 guard let freeScheme else {
@@ -397,7 +409,7 @@ extension LCUtils {
                 
                 launchURL = URL(string: "\(freeScheme)://open-url?url=\(encodedStr)")!
                 LCUtils.appGroupUserDefault.set(freeScheme, forKey: "LCLaunchExtensionScheme")
-                LCUtils.appGroupUserDefault.set(appToLaunch.appInfo.relativeBundlePath, forKey: "LCLaunchExtensionBundleID")
+                LCUtils.appGroupUserDefault.set(appToLaunch.appInfo.relativeBundlePath ?? appToLaunch.bundleIdentifier, forKey: "LCLaunchExtensionBundleID")
                 LCUtils.appGroupUserDefault.set(Date.now, forKey: "LCLaunchExtensionLaunchDate")
                 onServerMessage?("JIT acquisition will continue in another LiveContainer.")
                 
